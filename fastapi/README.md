ba4b59e359a6fd4adcd1bb0fd710076bcfdc2af5
ryoheiszk <63033551+ryoheiszk@users.noreply.github.com>
2024-09-12 18:35:59 +0900

    init

---
+++ fastapi/README.md
@@ -0,0 +1,14 @@
+# FastAPI
+
+```bash
+python -m venv .venv
+```
+
+```bash
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+```bash
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+```
---
+++ fastapi/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Fast API",
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
